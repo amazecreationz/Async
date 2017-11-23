3e0bad369bc7e51dd482d8c69aafc3d8d475e78e
Anand Mohan <moghan.anand@gmail.com>
2017-11-23 12:52:45 +0530

    Initial commit

---
+++ app/src/main/java/com/amazecreationz/async/activity/SplashActivity.java
@@ -0,0 +1,12 @@
+package com.amazecreationz.async.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
